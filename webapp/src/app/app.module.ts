df27962277288f9a5bcd676fafab9f85e3da6b13
Davide Ghiotto <ghiot@CLU22568.replynet.prv>
2020-05-30 21:29:18 +0200

    created menù + home + not found

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
