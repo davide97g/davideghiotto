b3d1a7b95eb217bca76c3f474f523a9834d3a93e
Davide Ghiotto <ghiot@CLU22568.replynet.prv>
2020-07-11 19:29:56 +0200

    added cv download + topics radar chart

--- template/assets/js/main.js
+++ template/assets/js/main.js
@@ -131,11 +131,11 @@ function getAge() {
 	new Chart(document.getElementById('languagesChart'), {
 		type: 'radar',
 		data: {
-			labels: ['C++', 'Java', 'JavaScript', 'TypeScript', 'Python', 'SQL'],
+			labels: ['C++', 'Java', 'JavaScript', 'TypeScript', 'Python', 'SQL', 'R'],
 			datasets: [
 				{
 					label: 'Programming Languages',
-					data: [90, 60, 90, 95, 30, 60],
+					data: [9, 6, 9, 9.5, 3, 6, 5],
 					fill: true,
 					backgroundColor: 'rgba(255, 99, 132, 0.2)',
 					borderColor: 'rgb(255, 99, 132)',
@@ -147,14 +147,15 @@ function getAge() {
 			],
 		},
 		options: {
-			elements: { line: { tension: 0, borderWidth: 3 } },
+			elements: { line: { tension: 0, borderWidth: 2 } },
 			scale: {
 				angleLines: {
 					display: false,
 				},
 				ticks: {
+					display: false,
 					suggestedMin: 0,
-					suggestedMax: 100,
+					suggestedMax: 10,
 				},
 			},
 			// legend: {
@@ -163,4 +164,39 @@ function getAge() {
 		},
 	});
 
+	new Chart(document.getElementById('topicsChart'), {
+		type: 'radar',
+		data: {
+			labels: ['Algorithms', 'Statistics', 'Machine Learning', 'Database', 'Deep Learning'],
+			datasets: [
+				{
+					label: 'Data Science Topics',
+					data: [9.5, 7, 8, 8, 5],
+					fill: true,
+					backgroundColor: 'rgba(54, 162, 235, 0.2)',
+					borderColor: 'rgb(54, 162, 235)',
+					pointBackgroundColor: 'rgb(54, 162, 235)',
+					pointBorderColor: '#fff',
+					pointHoverBackgroundColor: '#fff',
+					pointHoverBorderColor: 'rgb(54, 162, 235)',
+				},
+			],
+		},
+		options: {
+			elements: { line: { tension: 0, borderWidth: 2 } },
+			scale: {
+				angleLines: {
+					display: false,
+				},
+				ticks: {
+					display: false,
+					suggestedMin: 0,
+					suggestedMax: 10,
+				},
+			},
+			// legend: {
+			// 	display: false,
+			// },
+		},
+	});
 })(jQuery);
