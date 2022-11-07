7ab21e32c83dbee49e5f5fc791ed7541a0a2a543
Davide Ghiotto <dghiotto.dev@gmail.com>
2022-11-07 17:48:44 +0100

    background

--- vue-webapp/src/components/HelloWorld.vue
+++ vue-webapp/src/components/HelloWorld.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-	<h1>Hello World Component</h1>
+	<div
+		style="
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			height: 100%;
+			width: 100%;
+		"
+	>
+		<h1 style="color: white; font-size: 22px">Work In Progress</h1>
+	</div>
 </template>
-
-<style scoped></style>
