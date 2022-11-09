8d2f27b9da142209f7262fc5142ed88ae49ff15d
davide-ghiotto-infodati <davide.ghiotto@infodati.it>
2022-11-09 13:01:09 +0100

    router setup & pages

--- vue-webapp/src/App.vue
+++ vue-webapp/src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Footer from './components/Footer.vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 </script>
 
 <template>
-	<HelloWorld />
+	<Header />
+	<router-view></router-view>
 	<Footer />
 </template>
---
+++ vue-webapp/src/pages/About.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+	<h1>About me</h1>
+</template>
